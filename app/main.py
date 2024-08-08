d6b382788f1fdfb18946e3ab6b15f1f86cb30ff0
TonThanyathon <tonkingrat@gmail.com>
2024-08-08 22:54:21 +0700

    edit main.py

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ import sys
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 from faceparsing import process_image
-from model import predict_image
+from model import predict_image, load_model
 import logging
 import numpy as np
 import torch
@@ -30,10 +30,6 @@ logger = logging.getLogger(__name__)
 # Initialize MTCNN
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 mtcnn = MTCNN(keep_all=True, device=device)
-
-# Ensure 'uploads' directory exists
-if not os.path.exists('uploads'):
-    os.makedirs('uploads')
 
 @app.post("/check_face")
 async def check_face(image: UploadFile = File(...)):
@@ -82,7 +78,7 @@ async def predict(image: UploadFile = File(...)):
         logger.info(f"Segmented image saved to {processed_image_path}")
 
         prediction = predict_image(np.array(segmented_image))
-        logger.info(f"Prediction completed with re+sult: {prediction}") 
+        logger.info(f"Prediction completed with result: {prediction}") 
 
         result = {"prediction": int(prediction)}
         return JSONResponse(content=result)
@@ -92,4 +88,4 @@ async def predict(image: UploadFile = File(...)):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=5000) 
+    uvicorn.run(app, host="0.0.0.0", port=5000)
