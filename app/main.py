1fef4c008f11c77bea008a2e6904b824679832b7
TonThanyathon <tonkingrat@gmail.com>
2024-08-09 04:17:41 +0700

    update host main.py

--- app/main.py
+++ app/main.py
@@ -91,6 +91,10 @@ async def predict(image: UploadFile = File(...)):
         logger.error(f"An error occurred: {e}")
         return JSONResponse(status_code=500, content={"error": str(e)})
 
+# if __name__ == "__main__":
+#     import uvicorn
+#     uvicorn.run(app, host="0.0.0.0", port=5000)
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=5000)
+    uvicorn.run(app, host="127.0.0.1", port=5000)
+
